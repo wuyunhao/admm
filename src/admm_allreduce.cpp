c5df47b8bb2a5c07dd2ffc9803781770f9a1319b
yunhao1 <yunhao1@staff.sina.com.cn>
2015-09-10 21:45:33 +0800

    ..

---
+++ src/admm_allreduce.cpp
@@ -0,0 +1,112 @@
+#include <memory>
+#include <cstdlib>
+#include <rabit.h>
+#include <dmlc/logging.h>
+#include <dmlc/io.h>
+#include "sample_set.h"
+#include "workers.h"
+#include "master.h"
+#include "config.h"
+
+using namespace dmlc;
+using namespace rabit;
+class LocalModel : public dmlc::Serializable {
+  public:
+   ::admm::Worker worker_processor_;
+
+   void Load(dmlc::Stream *fi) {
+   }
+   void Save(dmlc::Stream *fo) const {
+     fo->Write(worker_processor_.base_vec_);
+   }
+   void InitModel(std::size_t fdim) {
+     worker_processor_.InitWorker(fdim);
+   }
+};
+
+class GlobalModel : public dmlc::Serializable {
+  public:
+   ::admm::AdmmConfig admm_params_;
+
+   void Load(dmlc::Stream *fi) {
+     fi->Read(&admm_params_.step_size, sizeof(admm_params_.step_size));
+     fi->Read(&admm_params_.global_var, sizeof(admm_params_.global_var));
+     fi->Read(&admm_params_.bias_var, sizeof(admm_params_.bias_var));
+     fi->Read(&admm_params_.dim, sizeof(admm_params_.dim));
+     fi->Read(&admm_params_.global_weights);
+   }
+   void Save(dmlc::Stream *fo) const {
+     fo->Write(&admm_params_.step_size, sizeof(admm_params_.step_size));
+     fo->Write(&admm_params_.global_var, sizeof(admm_params_.global_var));
+     fo->Write(&admm_params_.bias_var, sizeof(admm_params_.bias_var));
+     fo->Write(&admm_params_.dim, sizeof(admm_params_.dim));
+     fo->Write(admm_params_.global_weights);
+   }
+   void InitModel(float step_size,
+                  float global_var,
+                  float bias_var,
+                  std::size_t dim) {
+     admm_params_.Init(step_size, global_var, bias_var, dim);
+   }
+};
+int main(int argc, char* argv[]) {
+  InitLogging(argv[0]);
+  rabit::Init(argc, argv);
+  
+  LocalModel local_model;
+  GlobalModel global_model;
+  ::admm::SampleSet sample_set;
+  CHECK(sample_set.Initialize(argv[1], rabit::GetRank(), rabit::GetWorldSize()));
+
+  int max_iter = 1;
+  int iter = rabit::LoadCheckPoint(&global_model, &local_model);
+  if (iter == 0) {
+    global_model.InitModel(atof(argv[2]),
+                           atof(argv[3]),
+                           atof(argv[4]),
+                           atoi(argv[5]));
+    local_model.InitModel(atoi(argv[5]));
+  }
+
+  std::size_t dim = global_model.admm_params_.dim;
+  ::admm::Master master_processor;
+  std::vector<float> tmp;
+
+  for (int r = iter; r < max_iter; ++r) {
+    tmp.resize(dim);
+    std::fill(tmp.begin(), tmp.end(), 0.0f);   
+
+    auto lazy_ftrl = [&]()
+    {
+      local_model.worker_processor_.BiasUpdate(sample_set, global_model.admm_params_);
+      local_model.worker_processor_.BaseUpdate(sample_set, global_model.admm_params_);
+      local_model.worker_processor_.GetWeights(global_model.admm_params_, tmp);
+    };
+
+    LOG(INFO) << "the " << rabit::GetRank() << " reduce begins " << rabit::VersionNumber() << "\n";
+    rabit::Allreduce<op::Sum>(&tmp[0], tmp.size(), lazy_ftrl);
+    LOG(INFO) << "the " << rabit::GetRank() << " reduce ends " << rabit::VersionNumber() << "\n";
+
+    if (rabit::GetRank() == 0) {
+        master_processor.GlobalUpdate(tmp, global_model.admm_params_, rabit::GetWorldSize());
+    }
+    LOG(INFO) << "the " << rabit::VersionNumber()  << "boradcast begins" << "\n";
+    rabit::Broadcast(&(global_model.admm_params_.global_weights), 0);
+    LOG(INFO) << "the " << rabit::VersionNumber()  << "boradcast ends" << "\n";
+
+    local_model.worker_processor_.LangrangeUpdate(sample_set, global_model.admm_params_);
+    rabit::CheckPoint(&global_model, &local_model);
+    if (rabit::GetRank() == 0) {
+      rabit::TrackerPrintf("Finish %d-th iteration\n", r);
+    }
+  }
+  std::string filename = "processor_" + std::to_string(rabit::GetRank());
+  auto *stream = dmlc::Stream::Create(&filename[0], "w");
+  local_model.Save(stream);
+  if (rabit::GetRank() == 0) {
+    auto *streama(dmlc::Stream::Create("allprocesses.txt", "w"));
+    global_model.Save(streama);
+  }
+
+  rabit::Finalize();
+}
