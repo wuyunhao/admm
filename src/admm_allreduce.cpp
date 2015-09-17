ceea828e2bcab0c5ac0faa36fc0c6f2b1e42f832
yunhao1 <yunhao1@staff.sina.com.cn>
2015-09-17 18:55:55 +0800

    fix allreduce main.cpp

--- src/admm_allreduce.cpp
+++ src/admm_allreduce.cpp
@@ -10,6 +10,17 @@
 
 using namespace dmlc;
 using namespace rabit;
+
+//typedef std::size_t IndexType;
+//typedef ::dmlc::Row<IndexType> Row;
+
+float Predict(dmlc::Row<std::size_t>& x, std::vector<float>& weight_vec) {
+  auto* ptr_weight = &weight_vec[0];
+  auto inner_product = x.SDot(ptr_weight, weight_vec.size());
+
+  return 1.0/(1 + exp(- std::max(std::min(inner_product, (float)35), (float)(-35)))); 
+}
+
 class LocalModel : public dmlc::Serializable {
   public:
    ::admm::Worker worker_processor_;
@@ -19,15 +30,15 @@ class LocalModel : public dmlc::Serializable {
    void Save(dmlc::Stream *fo) const {
      dmlc::ostream os(fo);
      for (size_t i = 0; i < worker_processor_.base_vec_.size(); ++i) {
-        os << worker_processor_.base_vec_[i] << ' ';
+       os << worker_processor_.base_vec_[i] << ' ';
      }
      os << '\n';
      for (size_t i = 0; i < worker_processor_.bias_vec_.size(); ++i) {
-        os << worker_processor_.bias_vec_[i] << ' ';
+       os << worker_processor_.bias_vec_[i] << ' ';
      }
      os << '\n';
      for (size_t i = 0; i < worker_processor_.langr_vec_.size(); ++i) {
-        os << worker_processor_.langr_vec_[i] << ' ';
+       os << worker_processor_.langr_vec_[i] << ' ';
      }
 
    }
@@ -41,11 +52,6 @@ class GlobalModel : public dmlc::Serializable {
    ::admm::AdmmConfig admm_params_;
 
    void Load(dmlc::Stream *fi) {
-     fi->Read(&admm_params_.step_size, sizeof(admm_params_.step_size));
-     fi->Read(&admm_params_.global_var, sizeof(admm_params_.global_var));
-     fi->Read(&admm_params_.bias_var, sizeof(admm_params_.bias_var));
-     fi->Read(&admm_params_.dim, sizeof(admm_params_.dim));
-     fi->Read(&admm_params_.global_weights);
    }
    void Save(dmlc::Stream *fo) const {
      dmlc::ostream os(fo);
@@ -54,7 +60,21 @@ class GlobalModel : public dmlc::Serializable {
      os << admm_params_.bias_var << '\n';
      os << admm_params_.dim << '\n';
      for (size_t i = 0; i < admm_params_.dim; ++i) {
-        os << admm_params_.global_weights[i] << ' ';
+       os << admm_params_.global_weights[i] << ' ';
+     }
+   }
+   void SaveAuc(dmlc::Stream *fo, ::admm::SampleSet &sample_set ) {
+     dmlc::ostream os(fo);
+     sample_set.Rewind();
+     while(sample_set.Next()) {
+       dmlc::Row<std::size_t> x = sample_set.GetData();
+       os << x.label << ' ';
+     }
+     os << '\n';
+     sample_set.Rewind();
+     while(sample_set.Next()) {
+       dmlc::Row<std::size_t> x = sample_set.GetData();
+       os << Predict(x, admm_params_.global_weights) << ' ';
      }
    }
    void InitModel(float step_size,
@@ -64,6 +84,7 @@ class GlobalModel : public dmlc::Serializable {
      admm_params_.Init(step_size, global_var, bias_var, dim);
    }
 };
+
 int main(int argc, char* argv[]) {
   InitLogging(argv[0]);
   rabit::Init(argc, argv);
@@ -73,7 +94,7 @@ int main(int argc, char* argv[]) {
   ::admm::SampleSet sample_set;
   CHECK(sample_set.Initialize(argv[1], rabit::GetRank(), rabit::GetWorldSize()));
 
-  int max_iter = 1;
+  int max_iter = 3;
   int iter = rabit::LoadCheckPoint(&global_model);
   if (iter == 0) {
     global_model.InitModel(atof(argv[2]),
@@ -83,7 +104,7 @@ int main(int argc, char* argv[]) {
     local_model.InitModel(atoi(argv[5]));
   }
 
-  rabit::TrackerPrintf("Initialization finished");
+  rabit::TrackerPrintf("Initialization finished\n");
 
   std::size_t dim = global_model.admm_params_.dim;
   ::admm::Master master_processor;
@@ -100,17 +121,17 @@ int main(int argc, char* argv[]) {
       local_model.worker_processor_.GetWeights(global_model.admm_params_, tmp);
     };
 
-    rabit::TrackerPrintf("allreduce begined");
+    rabit::TrackerPrintf("allreduce begined\n");
 
     LOG(INFO) << "the " << rabit::GetRank() << " reduce begins " << rabit::VersionNumber() << "\n";
     rabit::Allreduce<op::Sum>(&tmp[0], tmp.size(), lazy_ftrl);
     LOG(INFO) << "the " << rabit::GetRank() << " reduce ends " << rabit::VersionNumber() << "\n";
 
-    rabit::TrackerPrintf("allreduce finished");
+    rabit::TrackerPrintf("allreduce finished\n");
 
-    if (rabit::GetRank() == 0) {
-        master_processor.GlobalUpdate(tmp, global_model.admm_params_, rabit::GetWorldSize());
-    }
+    //if (rabit::GetRank() == 0) {
+    master_processor.GlobalUpdate(tmp, global_model.admm_params_, rabit::GetWorldSize());
+    //}
     //rabit::Broadcast(&(global_model.admm_params_.global_weights), 0);
 
     local_model.worker_processor_.LangrangeUpdate(sample_set, global_model.admm_params_);
@@ -120,20 +141,25 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  rabit::TrackerPrintf("len of base weight is %f \n", local_model.worker_processor_.base_vec_[0]);
-
   std::string path = "hdfs://ns1/user/yunhao1/admm/";
-  std::string filename = "processor_" + std::to_string(rabit::GetRank());
-  auto *stream = dmlc::Stream::Create(&filename[0], "w");
+
+  std::string local_file = path + "processor_" + std::to_string(rabit::GetRank());
+  auto *stream = dmlc::Stream::Create(&local_file[0], "w");
   local_model.Save(stream);
   delete stream;
   if (rabit::GetRank() == 0) {
-    std::string result = "allprocesses.txt";
-    auto *streama(dmlc::Stream::Create(&result[0], "w"));
+    std::string global_file = path + "allprocesses.txt";
+    auto *streama(dmlc::Stream::Create(&global_file[0], "w"));
     global_model.Save(streama);
     delete streama;
-
-    rabit::TrackerPrintf("len of global weight is %f \n", global_model.admm_params_.global_weights[0]);
+    
+    std::string auc = path + "auc.txt";
+    auto *streamb(dmlc::Stream::Create(&auc[0], "w"));
+    //get the test set
+    ::admm::SampleSet test_set;
+    CHECK(test_set.Initialize(argv[6], rabit::GetRank(),1)); 
+    global_model.SaveAuc(streamb, test_set);
+    delete streamb;
   }
 
   rabit::Finalize();
